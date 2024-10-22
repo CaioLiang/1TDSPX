8d125a1e8bebbf6c63cd82a3536cbccf4ea99a82
CaioLiang <caioliang2@gmail.com>
2024-10-21 22:25:36 -0300

    adicionando arquivo DML de insert

---
+++ DML.sql
@@ -0,0 +1,17 @@
+INSERT INTO tb_endereco (
+    rua,
+    numero,
+    cep,
+    bairro,
+    cidade,
+    pais,
+    complemento
+) VALUES (
+    'AVENIDA SEM FREIO',
+    303,
+    '885512',
+    'PIRAPORINHA',
+    'AMAZANOS',
+    'CENTRO NORTE',
+    'USP-LEST'
+);
